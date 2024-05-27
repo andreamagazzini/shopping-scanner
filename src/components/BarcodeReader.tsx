b2f3fa6a8e91511ec9e6a4205d46db0b05fa2998
andreamagazzini <andreamaga4@gmail.com>
2024-05-27 18:01:46 +0200

    Add loading and error handler

--- src/components/BarcodeReader.tsx
+++ src/components/BarcodeReader.tsx
@@ -9,20 +9,34 @@ interface Props {
 
 const BarcodeReader: FC<Props> = ({ onDetected }) => {
   const [code, setCode] = useState<string>("")
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState<Error | unknown>()
 
   const getProductDetails = useCallback(async (code: string) => {
     const { data } = await axios.get(`/api/barcode/search?code=${code}`)
     data.product && onDetected(data.product)
+    setLoading(false)
   }, [onDetected])
 
   const { ref } = useZxing({
     onDecodeResult(result) {
-      console.log(result)
+      setError(null)
+      setLoading(true)
       if (result.getText() && code !== result.getText()) {
         setCode(result.getText())
         getProductDetails(result.getText())
+      } else {
+        setLoading(false)
       }
     },
+    onDecodeError(error) {
+      setError(error)
+      console.log(error)
+    },
+    onError(error) {
+      setError(error)
+      console.log(error)
+    }
   });
 
   return (
@@ -31,6 +45,14 @@ const BarcodeReader: FC<Props> = ({ onDetected }) => {
         <video id="camera-stream" ref={ref} />
         <canvas id="screenshot" className="hidden" />
       </div>
+      {
+        loading &&
+        <div>Loading...</div>
+      }
+      {
+        error && 
+        <div>{JSON.stringify(error)}</div>
+      }
     </>
   )
 }
